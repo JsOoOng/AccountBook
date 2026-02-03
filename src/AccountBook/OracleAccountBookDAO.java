b660749dadd3f41606954ce471fbd80db08c62d4
jsOoOng <qkr030503@naver.com>
2026-02-03 17:07:28 +0900

    findAll 병합 전 로컬 코드 저장

--- src/AccountBook/OracleAccountBookDAO.java
+++ src/AccountBook/OracleAccountBookDAO.java
@@ -119,6 +119,7 @@ public class OracleAccountBookDAO implements AccountBookDAO {
                 ab.setAmount(rs.getInt("amount"));
                 ab.setCategory(rs.getString("category"));
                 ab.setDate(rs.getString("indate")); // 컬럼명 확인 필요 (indate?)
+                rs.close(); ps.close();
                 return ab;
             }
             return null;
@@ -139,4 +140,45 @@ public class OracleAccountBookDAO implements AccountBookDAO {
             return 0;
         }
     }
+    @Override
+	public int update(AccountBook ab) {
+		try {
+			//기존에 값에 새로운 값을 교체하는 작업
+			AccountBook new_ab = findById(ab.getId());
+			if(ab.getType()!=null) {
+				new_ab.setType(ab.getType());
+			}
+			if(ab.getAmount()!=0) {
+				new_ab.setAmount(ab.getAmount());
+			}
+			if(ab.getCategory()!=null) {
+				new_ab.setCategory(ab.getCategory());
+			}
+			
+			if(ab.getDate()!=null) {
+				new_ab.setDate(ab.getDate());
+			}
+				
+			String sql = "update accountbook set type=?, amount=?, category=?, indate=? where id=?";
+			PreparedStatement ps = conn.prepareStatement(sql);
+
+			ps.setString(1, new_ab.getType());
+			
+			ps.setInt(2, new_ab.getAmount());
+			
+			ps.setString(3, new_ab.getCategory());
+			
+			ps.setString(4, new_ab.getDate());
+			
+			ps.setInt(5, new_ab.getId());
+			
+			int result = ps.executeUpdate(); //Statement 객체와 차이점은 sql을 여기서 실행하는것이 아님
+			
+			ps.close();
+			return result;
+		}catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
 }
